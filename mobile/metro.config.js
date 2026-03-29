721e16e2af1a9292f4b8a0ec4e1f89b308695a29
bterhart <bert.terhart@gmail.com>
2026-03-29 16:23:02 -0700

    Refactor Metro config to remove Vibecode integration
    Removed Vibecode integration from Metro config and updated module export.

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -3,7 +3,6 @@
 
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
-const { withVibecodeMetro } = require("@vibecodeapp/sdk/metro");
 const path = require("path");
 const fs = require("fs");
 
@@ -29,9 +28,10 @@ config.resolver.useWatchman = false;
 // Configure asset and source extensions.
 const { assetExts, sourceExts } = config.resolver;
 
-// SVG transformer is configured by withVibecodeMetro
+// Configure SVG transformer and build options
 config.transformer = {
   ...config.transformer,
+  babelTransformerPath: require.resolve("react-native-svg-transformer"),
   getTransformOptions: async () => ({
     transform: {
       experimentalImportSupport: false,
@@ -126,4 +126,4 @@ config.resolver = {
 };
 
 // Integrate NativeWind with the Metro configuration.
-module.exports = withNativeWind(withVibecodeMetro(config), { input: "./global.css" });
+module.exports = withNativeWind(config, { input: "./global.css" });
